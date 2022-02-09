2110bb9c43b4a8d8150c2ba27300ce4495e9a0d0
LuckyDog <928301810@qq.com>
2022-02-09 21:07:57 +0800

    commit

--- src/git_demo.java
+++ src/git_demo.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class git_demo {
     public static void main(String[] args) {
         System.out.println("master test");
-        int[] num = new int[10];
+//        int[] num = new int[10];  不要
         System.out.println("hot-fix test");
         List<String> list = new ArrayList<>();
     }
