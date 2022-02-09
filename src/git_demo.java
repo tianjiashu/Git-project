0a310be570ea1e3c12c06a3ebb86da9419effc82
LuckyDog <928301810@qq.com>
2022-02-09 17:36:41 +0800

    second

--- src/git_demo.java
+++ src/git_demo.java
@@ -1,2 +1,5 @@
 public class git_demo {
+    public static void main(String[] args) {
+
+    }
 }
--- src/git_test.java
+++ src/git_test.java
@@ -1,5 +1,5 @@
 public class git_test {
     public static void main(String[] args) {
-
+        System.out.println("hello git");
     }
 }
