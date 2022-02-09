ad66c7777ff5e4a9b39c7b72b2eafb46e6be90a4
LuckyDog <928301810@qq.com>
2022-02-09 18:18:13 +0800

    got-fix commit

--- src/git_demo.java
+++ src/git_demo.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class git_demo {
     public static void main(String[] args) {
-
+        System.out.println("hot-fix test");
+        List<String> list = new ArrayList<>();
     }
 }
